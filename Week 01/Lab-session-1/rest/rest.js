d857d65c78a2dcb7ed92bfaf838e2b599816527d
Seemab <seemabqureshi1991@gmail.com>
2023-09-11 17:09:18 -0400

    First Commit

---
+++ Week 01/Lab-session-1/rest/rest.js
@@ -0,0 +1,11 @@
+function userFriends(user, ...friends) {
+    console.log(user + ' has ' + friends.length + ' friends');
+ }
+ userFriends('User', 'Bob', 'Alice');
+
+
+ function abc(fname, lname){
+    alert(fname + lname)
+ }
+
+ abc("Faisal ", "Iqbal", "abc")
